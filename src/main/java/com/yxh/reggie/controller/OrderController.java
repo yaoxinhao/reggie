b10acb09fefabf6a02f70a408335bc6f236f2c4f
yxh <yxh@git.com>
2023-07-09 23:59:22 +0800

    second

--- src/main/java/com/yxh/reggie/controller/OrderController.java
+++ src/main/java/com/yxh/reggie/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.yxh.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yxh.reggie.common.R;
 import com.yxh.reggie.entity.Orders;
@@ -17,11 +18,27 @@ import java.time.LocalDateTime;
 public class OrderController {
     @Autowired
     private OrdersService ordersService;
+
+    /**
+     * 提交订单
+     * @param orders
+     * @return
+     */
     @RequestMapping("/submit")
     public R<String> submit (@RequestBody Orders orders){
         ordersService.submit(orders);
         return R.success("订单提交成功");
     }
+
+    /**
+     * 订单明细
+     * @param page
+     * @param pageSize
+     * @param number
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
     @GetMapping ("/page")
     public R<Page> page(int page, int pageSize, Long number,  LocalDateTime beginTime, LocalDateTime endTime){
         //加@requestparam表示传过的参数必须有这些，除非设置为false
@@ -32,5 +49,18 @@ public class OrderController {
         wrapper.orderByDesc(Orders::getOrderTime);
         ordersService.page(ordersPage,wrapper);
         return R.success(ordersPage);
+    }
+
+    /**
+     * 修改状态
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Orders orders){
+        LambdaUpdateWrapper<Orders> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(Orders::getId,orders.getId()).set(Orders::getStatus,orders.getStatus());
+        ordersService.update(wrapper);
+        return R.success("派送成功");
     }
 }
